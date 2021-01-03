9bb70ee1fcb7b0211c3064acc018bc5eee3d0932
Paulo Avramenko <pavlo.avramenko@moteefe.com>
2021-01-03 18:32:35 +0000

    feat: animation framer motion

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -2,29 +2,40 @@ import Link from 'next/link'
 import { Container, Card } from 'react-bootstrap'
 import { FormatDate } from 'components/format-date'
 import { Post } from 'models/post'
+import { motion } from 'framer-motion'
 interface Props {
   posts: Post[]
+}
+
+const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] }
+
+const frameVariants = {
+  hover: { scale: 0.95 }
 }
 
 const Feed = ({ posts }: Props) => (
   <Container>
     {posts.map(({ id, title, publish_date, description, author, slug }) => (
-      <Link key={id} href={slug}>
-        <a className="link-unstyled">
-          <Card className="border-left-0 border-right-0 border-top-0 rounded-0">
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                <FormatDate publish_date={publish_date} />
-              </Card.Subtitle>
-              <Card.Text className="mb-1">{description}</Card.Text>
-              <Card.Subtitle className="text-right">
-                <small>{author}</small>
-              </Card.Subtitle>
-            </Card.Body>
-          </Card>
-        </a>
-      </Link>
+      <motion.div whileHover="hover" variants={frameVariants} transition={transition} key={id}>
+        <Link href={`/${slug}`}>
+          <a className="link-unstyled">
+            <Card className="border-left-0 border-right-0 border-top-0 rounded-0">
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  <FormatDate publish_date={publish_date} />
+                </Card.Subtitle>
+                <Card.Text className="mb-1">{description}</Card.Text>
+                <Card.Subtitle className="text-right">
+                  <p>
+                    <small>{author}</small>
+                  </p>
+                </Card.Subtitle>
+              </Card.Body>
+            </Card>
+          </a>
+        </Link>
+      </motion.div>
     ))}
   </Container>
 )
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,13 +4,27 @@ import { Card, Button } from 'react-bootstrap'
 import { FormatDate } from 'components/format-date'
 import { CommentList } from 'components/comment-list'
 import { Post } from 'models/post'
+import { motion } from 'framer-motion'
 
 interface Props {
   post: Post
 }
 
+const variants = {
+  hidden: { opacity: 0.5, x: -250 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { delay: 0.1, duration: 0.4 }
+  },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut', duration: 1 }
+  }
+}
+
 const DetailPage = ({ post: { publish_date, title, content, author, id } }: Props) => (
-  <>
+  <motion.div initial="exit" animate="visible" exit="exit" variants={variants}>
     <Button variant="outline-light" className="mb-2">
       <Link href="/">
         <a>🔙</a>
@@ -22,18 +36,17 @@ const DetailPage = ({ post: { publish_date, title, content, author, id } }: Prop
       </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text className="text-justify">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: content
-            }}
-          />
-        </Card.Text>
+        <div
+          className="text-justify"
+          dangerouslySetInnerHTML={{
+            __html: content
+          }}
+        />
       </Card.Body>
       <Card.Footer className="text-muted text-right">{author}</Card.Footer>
     </Card>
     <CommentList postId={id} />
-  </>
+  </motion.div>
 )
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -52,7 +65,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const id = (params.slug as string).substring(params.slug.lastIndexOf('-') + 1)
-  console.log(id)
   const res = await fetch(`${process.env.API_URL}/posts/${id}`)
   const post = await res.json()
 
